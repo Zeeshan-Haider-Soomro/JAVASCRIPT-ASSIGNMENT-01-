51ceef7577fb86172a690ae01635099724023bf4
Zeeshan-Haider-Soomro <159009736+Zeeshan-Haider-Soomro@users.noreply.github.com>
2024-04-22 00:28:36 +0500

    chapter-01

---
+++ app.js
@@ -0,0 +1,41 @@
+// ALERT
+
+// QUESTION 01
+alert("Good Morning, Sunshine");
+
+// QUEATION 02
+alert("Error! Please enter a valid password");
+
+// QUEATION 03
+alert("Welcome to JS Land...\nHappy Coding!");
+
+// QUESTION 04 
+alert("Welcome to JS Land...");
+alert("Happy Coding!");
+
+// QUESTION 05
+alert("Hello...I can run JS through my web browser's console");
+console.log("Hello...I can run JS through my web browser's console");
+
+// SWAL
+
+// QUESTION 01
+Swal.fire("khairiyat tou hai, kahan te aap");
+
+// QUESTION 02 
+Swal.fire({title:"Are You Still Here?",text:"get out from my class!"});
+
+// QUESTION 03 
+Swal.fire({
+    title:"be aware this wesite is 18+",
+    text:"I'm over 18+"
+});
+
+// QUESTION 04
+Swal.fire("Unexpected error circulate\n404 ERROR!");
+
+// QUESTION 05
+Swal.fire({
+    title:"oops...",
+    text:"something went wrong!"
+});
